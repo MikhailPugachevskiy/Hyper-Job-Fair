7912ecef2a5b44bec8e170bc49f957baeb94a616
MikhailPugachevskiy <mikhailpugachevskiy@gmail.com>
2026-08-12 23:51:38 +0200

    created footer react

---
+++ hyper-job-fair/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+
+function Footer() {
+    return (
+        <footer>
+            <p>&copy; 2026 Hyper Job Fair – Personalisierte Jobmesse neu gedacht.</p>
+            <p>Basierend auf dem Konzept der 5I's (Identifikation, Individualisierung, Interaktion, Integration, Integrität).</p>
+        </footer>
+    )
+}
+
+export default Footer
